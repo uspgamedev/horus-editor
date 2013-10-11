aa194b080fac7c2c7d4f68d0180e7eb3d0cc1b1d
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-10-10 21:41:52 -0300

    Basic source code.

---
+++ src/editor.lua
@@ -0,0 +1,7 @@
+
+module ('editor', package.seeall)
+
+print "Welcome to Horus Eye's content editor."
+
+require 'editor.info'
+
---
+++ src/editor/info.lua
@@ -0,0 +1,18 @@
+
+module ('editor.info', package.seeall)
+
+function majorVersion ()
+  return 0
+end
+
+function minorVersion ()
+  return 0
+end
+
+function patchVersion ()
+  return 0
+end
+
+function version ()
+  return majorVersion().."."..minorVersion().."."..patchVersion()
+end
