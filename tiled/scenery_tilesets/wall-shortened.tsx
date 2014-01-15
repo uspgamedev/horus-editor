1ea228752d4a657ad85b8722ab9f68dd8b99265b
Renan T. Carneiro <renanteruoc@gmail.com>
2014-01-15 16:49:15 -0200

    Created tileset files and first level.

---
+++ tiled/scenery_tilesets/wall-shortened.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wall-shortened" tilewidth="106" tileheight="183">
+ <image source="../../resources/images/wall-shortened.png" width="106" height="183"/>
+</tileset>
