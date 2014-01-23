8d2a53b0af612a8f21cd4a5683cf0b9279e02536
Renan T. Carneiro <renanteruoc@gmail.com>
2014-01-23 17:22:30 -0200

    [Tiled] Added tilesets for switch, mummy and door

---
+++ tiled/object_tilesets/switch-inactive.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="switch-inactive" tilewidth="106" tileheight="106">
+ <image source="../../resources/images/switch/0001.png" width="106" height="106"/>
+</tileset>
